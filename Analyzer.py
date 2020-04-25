97b46dcaa6dfa2492f6b09a598bbabb180ab5bd1
Michael Wong <the.t.block@gmail.com>
2020-04-24 19:07:08 -0700

    refactored Analyzer to have consistent naming, and to use sets instead of lists, which makes the solve faster

--- Analyzer.py
+++ Analyzer.py
@@ -4,18 +4,23 @@ from PrefixTrie import PrefixTrie
 # whether they are valid words according to a supplied dictionary,
 # and for solving a grid.
 class Analyzer:
-
-    def __init__(self, validWords, language):
-        self.validWords = validWords
+    def __init__(self, valid_words, language):
+        self.valid_words = valid_words
         self.language = language
-        
-    # TODO: Determine whether neighbor setting is still necessary... can probably just have an implicit graph now
+  
+    # Configures the Analyzer with the current grid of letters.
+    # check and find_all_words will search the most recently set grid.
     def set_grid(self, grid):
-        # TODO: Rework
-        self.letters = grid
+        height, width = len(grid), len(grid[0])
+        # Collapse the grid to a 1D representation, as it simplifies
+        # the traversal algorithms.
+        self.letters = []
+        for row in grid:
+            self.letters.extend(row)
         self.adjList = [[] for _ in range(width * height)]
 
-        # add left and right neighbors
+        # Describe the grid as an adjacency list.
+        # Add left-right neighbor relationships.
         for i in range(height):
             for j in range(width - 1):
                 index = i * width + j
@@ -23,7 +28,7 @@ class Analyzer:
                 index = i * width + (j + 1)
                 self.adjList[index].append(index - 1)
 
-        # add top and bottom neighbors
+        # Add top-bottom neighbor relationships.
         for i in range(height - 1):
             for j in range(width):
                 index = i * width + j
@@ -31,7 +36,7 @@ class Analyzer:
                 index = (i + 1) * width + j
                 self.adjList[index].append(index - width)
 
-        # add diagonal neighbors
+        # Add diagonal neighbor relationships.
         for i in range(height - 1):
             for j in range(width - 1):
                 index = i * width + j
@@ -43,8 +48,7 @@ class Analyzer:
                 index = (i + 1) * width + (j + 1)
                 self.adjList[index].append(index - 1 - width)
 
-
-
+    # Returns True iff word is in valid_words and can be found in the grid.
     def check(self, word):
         # Helper depth-first-search function for traversing the grid. Returns True
         # iff string can be found in the grid starting at node, using only letters
@@ -83,7 +87,7 @@ class Analyzer:
             return False
 
         # If the word is in the dictionary, run the dfs from each cube to see if the word is in the grid.
-        if self.validWords.check(word):
+        if self.valid_words.check(word):
             for i in range(len(self.letters)):
                 if dfs(i, word, [False for _ in range(len(self.adjList))]):
                     return True
@@ -91,50 +95,49 @@ class Analyzer:
         # At this point, either the word is not in the dictionary, or it was not found in the grid.
         return False
 
-
-
+    # Returns a set of all valid words that can be found in the grid.
     def find_all_words(self):
         # Helper depth-first-search function for simultaneously traversing the grid 
-        # and the prefix trie. Returns a list of all (sub)strings rooted at trieNode
+        # and the prefix trie. Returns a list of all (sub)strings rooted at trie_node
         # in the prefix trie that can be found in the grid starting at node, using
         # only nodes not yet marked as visited.
-        def dfsTrieTraverse(node, visited, trieNode):   
-            result = []
-
+        def dfs_trie_traverse(node, visited, trie_node):   
             # Check to ensure that including this cube might still be contributing to a valid word.
-            # If not (i.e., if it does not exist in the PrefixTrie), we can return an empty list
+            # If not (i.e., if it does not exist in the PrefixTrie), we can return an empty set
             # to indicate that taking the cube at node will lead to no valid words.
             # If so, advance down the PrefixTrie to account for these letters and obtain
             # the updated trie node that can be passed to the recursive call.
             c = self.letters[node].lower()
             if len(c) == 1:
-                if c not in trieNode.children.keys():
-                    return []
-                nextNode = trieNode.children[c]
+                if c not in trie_node.children.keys():
+                    return set()
+                next_node = trie_node.children[c]
             elif len(c) == 2:  # Handle double letter cubes.
-                if c[0] not in trieNode.children.keys():
-                    return []
-                if c[1] not in trieNode.children[c[0]].children.keys():
-                    return []
-                nextNode = trieNode.children[c[0]].children[c[1]]
+                if c[0] not in trie_node.children.keys():
+                    return set()
+                if c[1] not in trie_node.children[c[0]].children.keys():
+                    return set()
+                next_node = trie_node.children[c[0]].children[c[1]]
 
-            # If we end up at a valid word, add it to the results.
-            if nextNode.valid:
-                result.append(c)
+            # Otherwise, we can reach a new trie node, so construct results via 
+            # recursive calls.
+            result = set()
+            # First, if we end up at a valid word, add it to the results.
+            if next_node.valid:
+                result.add(c)
 
             # DFS along the grid by visiting each neighboring cube that has not yet been visited.
             # Construct the result list by appending the current cube to the list returned by the 
             # subproblem recursive call.
             for neighbor in self.adjList[node]:
                 if not visited[neighbor]:
-                    suffixes = dfsTrieTraverse(neighbor, [True if i == node else visited[i] for i in range(len(visited))], nextNode)
-                    result.extend(map(lambda suffix: c + suffix, suffixes))
+                    suffixes = dfs_trie_traverse(neighbor, [True if i == node else visited[i] for i in range(len(visited))], next_node)
+                    result.update(map(lambda suffix: c + suffix, suffixes))
 
             return result
 
         # Accumulate a set of words by starting the DFS traversal from every cube.
-        words = []
+        words = set()
         for i in range(len(self.letters)):
-            words.extend(dfsTrieTraverse(i, [False for _ in range(len(self.adjList))], self.validWords.root))
-        return set(words)
-
+            words.update(dfs_trie_traverse(i, [False for _ in range(len(self.adjList))], self.valid_words.root))
+        return words
---
+++ AnalyzerTest.py
@@ -0,0 +1,20 @@
+import unittest
+from Analyzer import Analyzer
+from PrefixTrie import PrefixTrie
+
+class AnalyzerTest(unittest.TestCase):
+    def setUp(self):
+        lexicon = PrefixTrie("dictionaries/csw_en.txt")
+        self.analyzer = Analyzer(lexicon, "English")
+
+    def test_4x4_solve(self):
+        self.analyzer.set_grid([['A', 'T', 'Qu', 'V'],
+                                ['R', 'S', 'N', 'I'],
+                                ['X', 'I', 'S', 'E'],
+                                ['S', 'S', 'L', 'M']])
+        solution = self.analyzer.find_all_words()
+        expected_words = set(['ar', 'aril', 'arils', 'aris', 'arise', 'arisen', 'ars', 'arsine', 'arsines', 'arsis', 'art', 'arts', 'artsiness', 'as', 'asquint', 'ass', 'assent', 'at', 'ats', 'el', 'elint', 'elints', 'elm', 'elms', 'els', 'elsin', 'elsins', 'em', 'ems', 'en', 'ens', 'entasis', 'entrism', 'ents', 'es', 'ess', 'in', 'inisle', 'inisles', 'ins', 'insist', 'instar', 'intra', 'is', 'isle', 'isles', 'ism', 'issei', 'lei', 'leis', 'lenis', 'lens', 'lent', 'les', 'less', 'li', 'lin', 'line', 'lines', 'lins', 'lint', 'lints', 'lira', 'liras', 'lis', 'list', 'me', 'mein', 'meins', 'meint', 'mel', 'mels', 'men', 'mensa', 'ment', 'menta', 'mes', 'mess', 'ne', 'nelis', 'ness', 'nie', 'nies', 'nil', 'nils', 'nis', 'nisei', 'nisi', 'nisse', 'nix', 'quin', 'quine', 'quines', 'quins', 'quint', 'quinta', 'quintar', 'quintars', 'quintas', 'quints', 'ras', 'rasse', 'rassle', 'rast', 'rat', 'rats', 'rile', 'riles', 'rin', 'rine', 'rines', 'rins', 'rinse', 'rise', 'risen', 'sar', 'sari', 'sarin', 'sarins', 'saris', 'sat', 'sei', 'sel', 'sels', 'sen', 'sens', 'sensa', 'sensi', 'sensis', 'sent', 'sents', 'si', 'sien', 'siens', 'sient', 'sients', 'sile', 'silen', 'sileni', 'silens', 'silent', 'silents', 'siles', 'sin', 'sine', 'sines', 'sins', 'sir', 'sirs', 'sis', 'siss', 'sist', 'sista', 'sistra', 'six', 'snies', 'snirt', 'snirts', 'squint', 'sri', 'sris', 'st', 'star', 'strine', 'strines', 'ta', 'tar', 'tars', 'tarsi', 'tas', 'tass', 'tasse', 'tassel', 'tassels', 'tassie', 'trass', 'trin', 'trine', 'trines', 'trins', 'tsar', 'tsarism', 'vie', 'vies', 'vin', 'vine', 'vines', 'vins', 'vint', 'vints', 'vis', 'vise', 'visile', 'visne', 'xi', 'xis'])
+        self.assertEqual(solution, expected_words)
+
+if __name__ == "__main__":
+    unittest.main()
